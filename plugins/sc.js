47ae83c2d226daebcfdf5997b5893c1b2f40abdf
StorySad <bandars4bu03@gmail.com>
2022-10-17 12:49:58 +0700

    first commit

---
+++ plugins/sc.js
@@ -0,0 +1,19 @@
+let handler = async m => m.reply(`
+‹•══════════════♡᭄
+*TEMPAT RUN/AKTIF BOT*
+|  *•RUN BOT GRATIS |TANPA KENDALA*
+
+|  *•LINK SC* https://github.com/StorySad/Kang-MD
+
+|  *•LINK•* https://tinyurl.com/2gmkgndw
+
+|  *•TEPAT RUN DI PANEL*
+
+|  *•TEMPAT RUN/AKTIFIN BOT SECARA GERATIS TANPA KENDALA DI PANEL TINGGAL DAFTAR MASUKIN KODE REFERAL NYA KARNA ITU SUDAH ADA TINGGAL DAFTAR SUPAYA DAPET COIN UNTUK AKTIFIN BOT NYA*
+
+`.trim())// Tambah sendiri kalo mau
+handler.help = ['sc']
+handler.tags = ['main']
+handler.command = /^(sc|scrpits|scbot)$/i
+
+module.exports = handler
